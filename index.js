34a102dfb4d8f71fc5cd8c146bc86414f1e89dc3
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2023-12-26 20:14:30 +0200

    21

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-import {notice, info, success, error} from './node_modules/@pnotify/core/dist/PNotify.js';
-// const { alert, notice, info, success, error, defaultModules } = require('@pnotify/core/dist/PNotify.js')
-import * as PNotifyMobile from './node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
-defaultModules.set(PNotifyMobile, {});
-const myNotice = notice({
-text: "I'm a notice.",
-});
-const myInfo = info({
-text: "I'm an info message.",
-});
-const mySuccess = success({
-text: "I'm a success message.",
-});
-const myError = error({
-text: "I'm an error message.",
-});
+                                                        // ex 1
 const game = document.querySelector(".game")
 const btn = document.querySelector(".startBtn")
 const text = document.createElement("p")
 const keys = ["h", "d", "s", "q", "r", "t", "n", "g", "v", "x"]
-currentKeyIndex = 0;
+let currentKeyIndex = 0;
 let key = keys[currentKeyIndex];
 text.setAttribute("id", key)
 text.textContent = `Натисніть кнопку "${key}" щоб продовжити грати`;
@@ -27,18 +12,22 @@ window.addEventListener("keydown", checkBtn);
 function checkBtn(e) {
     if (key === e.key) {
     currentKeyIndex += 1
+        if (currentKeyIndex > keys.length - 1) {
+        PNotify.success({
+        text: "Ви перемогли",
+        delay: 2000,
+        });
+            text.textContent = "Перемога!!!";
+            return
+        }
         key = keys[currentKeyIndex]
         text.setAttribute("id", key)
         text.textContent = `Натисніть кнопку "${key}" щоб продовжити грати`;
-        if (currentKeyIndex > keys.length - 1
-        ) {
-            const mySuccess = success({
-            text: "You are winner",
-        });
-            text.textContent =  `1111`;
-        }
     } else {
-        alert("Ви натиснули не вірну кномпку")
+        PNotify.error({
+        text: "Ви натиснули не вірну клавішу",
+        delay: 2000,
+        });
  }
 }
 btn.addEventListener("click", start)
@@ -48,3 +37,26 @@ function start() {
     text.setAttribute("id", key)
     text.textContent = `Натисніть кнопку "${key}" щоб продовжити грати`;
 }
+                                                            // ex 2
+// import Chart from './node_modules/chart.js';
+// console.log(Chart)
+// const stats = document.querySelector("#sales-chart")
+// const chartData = {
+// labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'],
+// datasets: [{
+// label: 'Продажі за останній місяць',
+// data: [150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350],
+// backgroundColor: '#2196f3',
+// borderColor: '#2196f3',
+// borderWidth: 1
+// }]
+// };
+// const config = {
+// type: 'line',
+// data: data,
+// options: {}
+// };
+// const salesChart = new Chart(
+// stats,
+// config
+// );
