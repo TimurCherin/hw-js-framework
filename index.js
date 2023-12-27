98673dd1b24bf0b2fd03721c40af716799a7a2fa
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2023-12-27 15:18:23 +0200

    1

--- index.js
+++ index.js
@@ -39,24 +39,24 @@ function start() {
 }
                                                             // ex 2
 // import Chart from './node_modules/chart.js';
-// console.log(Chart)
-// const stats = document.querySelector("#sales-chart")
-// const chartData = {
-// labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'],
-// datasets: [{
-// label: 'Продажі за останній місяць',
-// data: [150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350],
-// backgroundColor: '#2196f3',
-// borderColor: '#2196f3',
-// borderWidth: 1
-// }]
-// };
-// const config = {
-// type: 'line',
-// data: data,
-// options: {}
-// };
-// const salesChart = new Chart(
-// stats,
-// config
-// );
+console.log(Chart)
+const stats = document.querySelector("#sales-chart")
+const chartData = {
+labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'],
+datasets: [{
+label: 'Продажі за останній місяць',
+data: [150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350],
+backgroundColor: '#2196f3',
+borderColor: '#2196f3',
+borderWidth: 1
+}]
+};
+const config = {
+type: 'line',
+data: chartData,
+options: {}
+};
+const salesChart = new Chart(
+stats,
+config
+);
